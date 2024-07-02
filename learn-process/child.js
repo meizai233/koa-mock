16b298c4dfc86fd28b2b49b04885764c0bfd16c3
fengwanyan <fengwy1@chinatelecom.cn>
2024-07-02 10:23:26 +0800

    feat: 父子进程通信2

--- learn-process/child.js
+++ learn-process/child.js
@@ -2,7 +2,9 @@
 process.on("message", function (m) {
   console.log("message from parent: " + JSON.stringify(m));
 });
+process.on("message", (msg) => {
+  console.log("子进程收到主进程的消息：", msg);
+});
 
-process.send({ from: "child" });
-
-console.log("process: ", process);
+// 给主进程发消息
+process.send("hello master process!");
---
+++ learn-process/command.js
@@ -0,0 +1,7 @@
+// command.js
+console.log(`pid: ${process.pid}, 进程${process.argv[2]}
+  的stdout`);
+// console.log("pprocess", process);
+
+console.error(`pid: ${process.pid}, 进程${process.argv[2]}
+  的stderr`);
